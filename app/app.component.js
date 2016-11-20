d2fcbe1a11a2871a430e9c9fca902cd70f9d414f
Jeeva Nadarajah <jeeva.ganeshan@gmail.com>
2016-11-20 01:25:45 -0500

    Built out the app-component to iterate through users

--- app/app.component.js
+++ app/app.component.js
@@ -11,11 +11,16 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.message = 'Hello!';
+        this.users = [{ id: 25, name: 'Lane', username: 'crazyLane' },
+            { id: 26, name: 'Henry', username: 'henryThe' },
+            { id: 27, name: 'Jennifer', username: 'jFur' }
+        ];
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <div class=\"jumbotron\">\n      <h1>Welcome to Insurance Marketplace!</h1>\n    </div>\n  ",
+            template: "\n    <header>\n        <nav class=\"navbar navbar-inverse\">\n            <div class=\"navbar-header\">\n                <a href=\"/\" class=\"navbar-brand\">Insurance Marketplace!</a>\n            </div>\n        </nav>\n    </header>\n    <div class=\"jumbotron\" >\n      <h1 align=\"center\">Welcome to</h1>\n      <h1 align=\"center\">Insurance Marketplace!</h1>\n      <div *ngIf=\"users\">\n          <div *ngFor=\"let user of users\">\n            {{user.name}} ({{user.username}})\n          </div>\n      </div>\n    </div>\n    \n    <footer class=\"text-center\">\n        Copyright &copy; 2016\n    </footer>\n    ",
             styles: ["\n    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }\n  "]
         }), 
         __metadata('design:paramtypes', [])
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,35 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-    <div class="jumbotron">
-      <h1>Welcome to Insurance Marketplace!</h1>
+    <header>
+        <nav class="navbar navbar-inverse">
+            <div class="navbar-header">
+                <a href="/" class="navbar-brand">Insurance Marketplace!</a>
+            </div>
+        </nav>
+    </header>
+    <div class="jumbotron" >
+      <h1 align="center">Welcome to</h1>
+      <h1 align="center">Insurance Marketplace!</h1>
+      <div *ngIf="users">
+          <div *ngFor="let user of users">
+            {{user.name}} ({{user.username}})
+          </div>
+      </div>
     </div>
-  `,
+    
+    <footer class="text-center">
+        Copyright &copy; 2016
+    </footer>
+    `,
     styles: [`
     .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
   `]
 })
-export class AppComponent {}
+export class AppComponent {
+    message = 'Hello!';
+    users = [{ id: 25, name: 'Lane', username: 'crazyLane'},
+        {id:26, name: 'Henry', username: 'henryThe'},
+        {id:27, name: 'Jennifer', username: 'jFur'}
+    ]
+}
